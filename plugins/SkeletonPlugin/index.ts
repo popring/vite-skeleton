897eeaee44cb1e32673e1413ac8128867effcd8b
popring <koler778@gmail.com>
2024-09-25 01:16:58 +0800

    feat: 优化代码注入方式；js入口文件改为async加载

--- plugins/SkeletonPlugin/index.ts
+++ plugins/SkeletonPlugin/index.ts
@@ -1,20 +1,27 @@
 import { PluginOption } from 'vite';
 import { join } from 'path';
-import { readFile } from 'node:fs/promises'
-import { load } from 'cheerio';
+import { readFile } from 'node:fs/promises';
 
 const filename = join(__dirname, './homeSkeleton.js');
-const code = await readFile(join(__dirname, './script.js'), { encoding: 'utf-8' })
+const code = await readFile(join(__dirname, './script.js'), {
+  encoding: 'utf-8',
+});
 
 export function SkeletonPlugin(): PluginOption {
   return {
     name: 'SkeletonPlugin',
     async transformIndexHtml(html) {
-      const modifiedHtml = html.replace('<script', '<script defer');
       const content = (await import(filename)).default;
-      const $ = load(modifiedHtml);
-      $('body').append(`<script>var map=${JSON.stringify(content)};${code}</script>`);
-      return $.html();
+      return {
+        html,
+        tags: [
+          {
+            tag: 'script',
+            injectTo: 'body-prepend',
+            children: `var map=${JSON.stringify(content)};${code}`,
+          },
+        ],
+      };
     },
   };
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import Pages from 'vite-plugin-pages';
-import { SkeletonPlugin} from './plugins/SkeletonPlugin'
+import { SkeletonPlugin } from './plugins/SkeletonPlugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
