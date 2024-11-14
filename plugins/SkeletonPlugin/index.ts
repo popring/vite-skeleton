c5ec3b85e5b75b8c78f22198727898598ad3f6af
popring <koler778@gmail.com>
2024-11-14 16:53:55 +0800

    feat: 初版方案

--- plugins/SkeletonPlugin/index.ts
+++ plugins/SkeletonPlugin/index.ts
@@ -1,27 +1,25 @@
 import { PluginOption } from 'vite';
 import { join } from 'path';
-import { readFile } from 'node:fs/promises';
 
 const filename = join(__dirname, './homeSkeleton.js');
-const code = await readFile(join(__dirname, './script.js'), {
-  encoding: 'utf-8',
-});
 
 export function SkeletonPlugin(): PluginOption {
   return {
     name: 'SkeletonPlugin',
     async transformIndexHtml(html) {
+      // 新增
+      const modifiedHtml = html.replace('<script', '<script defer');
       const content = (await import(filename)).default;
-      return {
-        html,
-        tags: [
-          {
-            tag: 'script',
-            injectTo: 'body',
-            children: `var map=${JSON.stringify(content)};${code}`,
-          },
-        ],
-      };
+      const code = `
+      <script id="skeleton-script">
+var map = ${JSON.stringify(content)}
+var pathname = window.location.pathname
+var target = map[pathname]
+var content = target && target.html || ''
+content && (document.querySelector('#skeleton-script').parentElement.innerHTML += content)
+</script>
+      `;
+      return modifiedHtml.replace(/__SKELETON_CONTENT__/, code);
     },
   };
 }
